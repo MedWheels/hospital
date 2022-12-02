46ba850c231e4819684112d075532f3bcdd12cb4
Rohith G <gangadharrohith6@gmail.com>
2022-12-02 22:02:36 +0530

    ,

--- utils/endpoints.js
+++ utils/endpoints.js
@@ -1,7 +1,7 @@
 const endpoints = {
 	//testing
 	// "getToken": "http://localhost:4000/api/hospitals/getToken?username=",
-	// "setToken": "http://localhost:4000/api/hospitals/setToken"
+	// "setToken": "http://localhost:4000/api/hospitals/setToken",
 	//production
 	"getToken": "https://medwheels.vercel.app/api/hospitals/getToken?username=",
 	"setToken": "https://medwheels.vercel.app/api/hospitals/setToken",
